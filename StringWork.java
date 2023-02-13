b6527e783a8ebea724d27d023433f7f85d9a3e5c
Gaurav <gaurav5186520@gmail.com>
2023-02-13 08:44:32 +0000

    first commit

---
+++ StringWork.java
@@ -0,0 +1,14 @@
+class StringWork {
+   
+public static void main(String[] args) {
+    String name1 = "Saurabh";
+    String name2 = "kumar";
+
+   System.out.println(name1.trim());
+   System.out.println(name1.indexOf('a'));
+   System.out.println(name1.lastIndexOf('a'));
+   System.out.println(name1.equalsIgnoreCase(name2));
+
+
+}    
+}
---
+++ firstProgram.java
@@ -0,0 +1,7 @@
+public class firstProgram{
+
+    public static void main(String[] args){
+
+        System.out.println("gaurav is hear");
+    }
+}
